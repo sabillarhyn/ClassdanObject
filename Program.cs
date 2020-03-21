454c3cfd86ec46862b88ccd6097b1a8a90952d0b
sabillarhyn <salsabilla.rohyani@students.amikom.ac.id>
2020-03-21 11:56:04 +0700

    program taxi

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TugasClassdanObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Taxi taxi = new Taxi();
+
+            taxi.DriverName = "Jono";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Taxi.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Taxi
+{
+	public string DriverName { get; set; }
+	public bool OnDuty { get; set; }
+	public int NumPassenger { get; set; }
+
+	public void TaxiInfo()
+	{
+
+		Console.WriteLine("Driver Name : {0}", DriverName);
+		if (OnDuty == true)
+			Console.WriteLine("On Duty : Yes");
+		else if (OnDuty == false)
+			Console.WriteLine("On Duty : No");
+		Console.WriteLine("Num of Passenger: {0}", NumPassenger);
+
+	}
+
+	public void PickUpPassenger()
+	{
+		Console.WriteLine("\n{0} sedang menjemput penumpang", DriverName);
+	}
+
+	public void DropOffPassenger()
+	{
+		Console.WriteLine("{0} selesai mengantar penumpang", DriverName);
+	}
+}
